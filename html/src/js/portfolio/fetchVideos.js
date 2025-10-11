a4d39edf5bb5401975cb2807aa1b913d9b48a92a
annaivv <annashalya@gmail.com>
2025-10-11 19:21:54 +0200

    feature: video title added to li

--- html/src/js/portfolio/fetchVideos.js
+++ html/src/js/portfolio/fetchVideos.js
@@ -1,7 +1,7 @@
 const exampleVideos = [
-  { id: 'CjO9JVlbQSM' },
-  { id: 'toJEvF6L1Yk' },
-  { id: 'UQPqg4gSSXc' },
+  { id: 'CjO9JVlbQSM', title: 'Video 1' },
+  { id: 'toJEvF6L1Yk', title: 'Video 2' },
+  { id: 'UQPqg4gSSXc', title: 'Video 3' },
 ];
 
 export async function fetchVideos() {
@@ -17,7 +17,12 @@ export async function fetchVideos() {
       'accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture';
     iframe.allowFullscreen = true;
 
+    const videoTitle = document.createElement('p');
+    videoTitle.className = 'video-title';
+    videoTitle.textContent = `${video.title}`;
+
     videoContainer.appendChild(iframe);
+    videoContainer.appendChild(videoTitle);
     videoList.appendChild(videoContainer);
   });
 }
