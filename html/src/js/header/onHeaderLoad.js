e3accffb970390ec470cb264782d4fa3285d09b5
Volodymyr <77401737+vova-rsk@users.noreply.github.com>
2025-10-19 21:23:53 +0200

    refactoring
    header-refactoring
    fixed position on PC-version

--- html/src/js/header/onHeaderLoad.js
+++ html/src/js/header/onHeaderLoad.js
@@ -1,7 +1,9 @@
 export async function onHeaderLoad() {
   toggleNavVisibility();
+  fixHeaderPosition();
 
   window.addEventListener('resize', toggleNavVisibility);
+  window.addEventListener('resize', fixHeaderPosition);
 }
 
 function toggleNavVisibility() {
@@ -16,5 +18,17 @@ function toggleNavVisibility() {
   } else {
     navRef.classList.remove('visually-hidden');
     menuBtnRef.classList.add('visually-hidden');
+  }
+}
+
+function fixHeaderPosition() {
+  const headerRef = document.getElementById('header');
+
+  if (!headerRef) return;
+
+  if (window.innerWidth >= 1200) {
+    const scrollbarWidth = window.innerWidth - document.documentElement.clientWidth;
+
+    headerRef.style.transform = `translateX(calc(-50% - ${scrollbarWidth/2}px))`;
   }
 }
