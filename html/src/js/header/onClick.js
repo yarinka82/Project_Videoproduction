631513bf9ce89ab382326b0dac3a1dce99d0d040
Volodymyr <77401737+vova-rsk@users.noreply.github.com>
2025-10-21 23:30:42 +0200

    fix header
    fix opening on pc-version and other bugs. temporary removed smooth opening/closing

--- html/src/js/header/onClick.js
+++ html/src/js/header/onClick.js
@@ -1,10 +1,7 @@
 export function onClick() {
   const menuRef = document.getElementById('header-menu');
-  const navRef = document.getElementById('nav');
 
   if (window.innerWidth < 1200) {
     menuRef.classList.toggle('isOpened');
   }
-
-  navRef.classList.toggle('visually-hidden');
 }
