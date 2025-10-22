a82238179ff85e971d1070e8075fe5629dca171a
Volodymyr <77401737+vova-rsk@users.noreply.github.com>
2025-10-22 20:49:00 +0200

    added section markup with styles

---
+++ html/src/js/problems/addMarkup.js
@@ -0,0 +1,8 @@
+function addMarkup(ref, tmp, data) {
+  const template = Handlebars.compile(tmp);
+  const markup = template(data);
+
+  ref.innerHTML = markup;
+}
+
+export default addMarkup;
--- html/src/js/problems/makeProblemsSection.js
+++ html/src/js/problems/makeProblemsSection.js
@@ -1,14 +1,14 @@
-import { cards } from '../../../db/data.js';
+import { cards } from '../../../db/problems.js';
 import loadTemplate from '../common/loadTemplate.js';
 import addMarkup from './addMarkup.js';
 import { startObserver } from '../common/observer.js';
 
-export async function makeAdvantagesSectionMarkup(containerId, tmpLink) {
+export async function makeProblemsSectionMarkup(containerId, tmpLink) {
   const ref = document.getElementById(containerId);
   const cardsTmp = await loadTemplate(tmpLink);
 
   if (!cards) return;
 
   addMarkup(ref, cardsTmp, cards);
-  startObserver('.slide-item');
+  // startObserver('.slide-item');
 }
