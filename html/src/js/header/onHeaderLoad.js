2e7e2991dfd457d5a5e8894b40e8b506ec7af18a
Volodymyr <77401737+vova-rsk@users.noreply.github.com>
2025-11-04 22:36:43 +0100

    refactoring

--- html/src/js/header/onHeaderLoad.js
+++ html/src/js/header/onHeaderLoad.js
@@ -1,21 +1,16 @@
 import { onHeaderMenuBtnClick } from "./onHeaderMenuBtnClick.js";
 import { onHeaderLinksClick } from "./onHeaderLinksClick.js";
 import { fixHeaderPosition } from "./fixHeaderPosition.js";
-import { toggleAdressDescriptionVisibility } from "./toggleAdressDescriptionVisibility.js";
 
 export async function onHeaderLoad() {
   const headerRef = document.getElementById('header');
-  const descriptionsRefs = document.querySelectorAll('.header-contacts-link-text');
 
   fixHeaderPosition(headerRef);
   onHeaderMenuBtnClick();
   onHeaderLinksClick();
-  toggleAdressDescriptionVisibility(descriptionsRefs);
 
   window.addEventListener('resize', ()=>fixHeaderPosition(headerRef));
   window.addEventListener('resize', onHeaderMenuBtnClick);
-  window.addEventListener('resize', ()=>toggleAdressDescriptionVisibility(descriptionsRefs));
-
 }
 
 
