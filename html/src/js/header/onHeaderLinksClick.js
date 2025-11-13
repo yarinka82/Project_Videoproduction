8e2db6eed11f6c8eb947cb106b54947a8b890bad
Petrushkov_Yevhenii <petrushkov093@gmail.com>
2025-11-13 15:02:24 +0200

    Revert "Merge pull request #40 from yarinka82/header/update"
    This reverts commit f9014d37b510f6cd9ffc9a1118f86bc9ffe09e8a, reversing
    changes made to 5efd8ca2157cda7e2a8c4ac879bab6f94dcc1c1d.

--- html/src/js/header/onHeaderLinksClick.js
+++ html/src/js/header/onHeaderLinksClick.js
@@ -2,13 +2,12 @@ import { onClick } from "./onClick.js";
 
 export function onHeaderLinksClick() {
   const navLinksRef = document.querySelectorAll('.header-navigation-link');
-  const toFormularLinkRef = document.querySelector('.header-formular-link');
+  const toFormularLink = document.querySelector('.header-formular-link');
   const contactsRef = document.querySelectorAll('.header-contacts-link');
-  const logoLinkRef = document.querySelector('.header-logo-link');
 
-  if (!navLinksRef || !toFormularLinkRef || !contactsRef || !logoLinkRef) return;
+  if (!navLinksRef || !toFormularLink || !contactsRef) return;
 
-  [...navLinksRef, toFormularLinkRef, ...contactsRef, logoLinkRef].forEach(link => {
+  [...navLinksRef, toFormularLink, ...contactsRef].forEach(link => {
     link.addEventListener('click', onClick);
   });
 }
