8139cc5e713526c150cfb36a4961a7f37beca562
Volodymyr <77401737+vova-rsk@users.noreply.github.com>
2025-11-13 22:39:37 +0100

    fix for menu-closing  by click logo
    fixed menu-avtoclosing on Mobile and Tablet.

--- html/src/js/header/onHeaderLinksClick.js
+++ html/src/js/header/onHeaderLinksClick.js
@@ -2,12 +2,13 @@ import { onClick } from "./onClick.js";
 
 export function onHeaderLinksClick() {
   const navLinksRef = document.querySelectorAll('.header-navigation-link');
-  const toFormularLink = document.querySelector('.header-formular-link');
-  const contactsRef = document.querySelectorAll('.header-contacts-link');
+  const toFormularLinkRef = document.querySelector('.header-formular-link');
+  const contactsRefs = document.querySelectorAll('.header-contacts-link');
+  const logoLinkRef = document.querySelector('.header-logo-link');
 
-  if (!navLinksRef || !toFormularLink || !contactsRef) return;
+  if (!navLinksRef || !toFormularLinkRef || !contactsRefs || !logoLinkRef) return;
 
-  [...navLinksRef, toFormularLink, ...contactsRef].forEach(link => {
+  [...navLinksRef, toFormularLinkRef, ...contactsRefs, logoLinkRef].forEach(link => {
     link.addEventListener('click', onClick);
   });
 }
