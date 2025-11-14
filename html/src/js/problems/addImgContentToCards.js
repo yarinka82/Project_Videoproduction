f52511675a10b61ae0d881ce15a4c3337adc158c
Volodymyr <77401737+vova-rsk@users.noreply.github.com>
2025-11-14 23:00:11 +0100

    refactoring addMarkup function

--- html/src/js/problems/addImgContentToCards.js
+++ html/src/js/problems/addImgContentToCards.js
@@ -1,4 +1,4 @@
-import addMarkup from "./addMarkup.js";
+import addMarkup from '../common/addMarkupFromTemplate.js';
 import loadTemplate from "../common/loadTemplate.js";
 
 export async function addImgContentToCard(refsArr, templatesLinksArr) {
@@ -7,7 +7,7 @@ export async function addImgContentToCard(refsArr, templatesLinksArr) {
   templatesLinksArr.forEach((tmpLink, i) => {
     try {
       loadTemplate(tmpLink).then(tmp => {
-        addMarkup(refsArr[i], tmp, []);
+      addMarkup(refsArr[i], tmp, []);
       });
     } catch (err) {
       throw new Error(err)
