039600d6ae65151e4b4c8ef94354e778398442cd
Volodymyr <77401737+vova-rsk@users.noreply.github.com>
2025-12-24 18:59:46 +0100

    update

--- html/db/problems.js
+++ html/db/problems.js
@@ -1,23 +1,26 @@
 export const cards = [
   {
-  id: 1,
-  image: 'someimage.img',
-  alternative:'image description',
-  title: 'Wir lösen das Problem der niedrigen Conversion-Rate',
-  description:'Kunden entscheiden sich für die, die sie zuerst sehen. \n Ein hochwertiges Video macht Ihr Unternehmen sichtbar und schafft Vertrauen – so werden Sie zur natürlichen Wahl, wenn Kunden nach einer Lösung suchen'
+    id: 1,
+    image: 'someimage.img',
+    alternative: 'image description',
+    title: 'Wir lösen das Problem der niedrigen Conversion-Rate',
+    description:
+      'Kunden entscheiden sich für die, die sie zuerst sehen. \n Ein hochwertiges Video macht dein Unternehmen sichtbar und schafft Vertrauen – so wirst du zur natürlichen Wahl, wenn Kunden nach einer Lösung suchen',
   },
-    {
-  id: 2,
-  image: 'someimage.img',
-  alternative:'image description',
-  title: 'Wir ziehen talentierte Fachkräfte an',
-  description:'Gute Fachkräfte schätzen nicht nur das Gehalt, sondern auch die Atmosphäre und die Werte des Unternehmens. \n Ein Video über Ihr Team zeigt, wer Sie wirklich sind – lebendige Gesichter, Energie, Werte. So ziehen Sie Menschen an, die Ihre Ziele teilen'
+  {
+    id: 2,
+    image: 'someimage.img',
+    alternative: 'image description',
+    title: 'Wir ziehen talentierte Fachkräfte an',
+    description:
+      'Gute Fachkräfte schätzen nicht nur das Gehalt, sondern auch die Atmosphäre und die Werte des Unternehmens. \n Ein Video über dein Team zeigt, wie – lebendige Gesichter, Energie, Werte. So ziehen Sie Menschen an, die Ihre Ziele teilen',
   },
-      {
-  id: 3,
-  image: 'someimage.img',
-  alternative:'image description',
-  title: 'Wir schaffen nachhaltige Ergebnisse',
-  description:'Ein einzelnes Video wirkt nur kurzfristig. \n Ein systematischer Einsatz von Videoinhalten schafft Vertrauen und Markenbekanntheit, zieht kontinuierlich Kunden an und stärkt Ihre Marktposition'
-}
+  {
+    id: 3,
+    image: 'someimage.img',
+    alternative: 'image description',
+    title: 'Wir schaffen nachhaltige Ergebnisse',
+    description:
+      'Ein einzelnes Video wirkt nur kurzfristig. \n Ein systematischer Einsatz von Videoinhalten schafft Vertrauen und Markenbekanntheit, zieht kontinuierlich Kunden an und stärkt Ihre Marktposition',
+  },
 ];
